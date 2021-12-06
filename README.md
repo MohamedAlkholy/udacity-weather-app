05596333d1ae09fb4cc80e18510aa887695e3cad
Mohamed <moh_monem43@hotmail.com>
2021-12-06 20:02:17 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Weather-Journal-App-Project
+
+## Description
+
+This project is the second project of udacity nanodegree front end with under Egypt Future Work is Digital Scholarship. starter code provided by udacity
+[landing page starter code](https://github.com/udacity/fend.git)
+
+## Table of Contents
+
+* [Project name](#Weather-Journal-App-Project)
+* [Description](#description)
+* [Instructions](#instructions)
+
+## Instructions
+
+* download repo from here [Weather-app repo](https://github.com/MohamedAlkholy/udacity-weather-app.git)
+* or clone repo using this link [Weather-app repo](https://github.com/MohamedAlkholy/udacity-weather-app.git)
+
+
---
+++ website/app.js
@@ -0,0 +1,99 @@
+/* Global Variables */
+const generateBtn = document.getElementById("generate");
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+
+// Personal API Key for OpenWeatherMap API
+APIKey = "24004ea06a8d8beb552f7612e67c3629";
+// URL
+// "api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={API key}&units=metric"
+
+// generateData function
+const generateData = async () => {
+  const zipCode = document.getElementById("zip").value;
+  const feelings = document.getElementById("feelings").value;
+
+  // call fetchData
+  const data = await fetchData(zipCode);
+  if (data) {
+    const {
+      main: { temp },
+      name,
+    } = data;
+    const finalData = {
+      newDate,
+      temp: Math.round(temp),
+      name,
+      feelings,
+    };
+    // call sendData to send finalData to server
+    sendData("/add", finalData);
+
+    // call getData that to retrieve data from server
+    getData();
+  }
+};
+
+// send finalData to server
+const sendData = async (url = "", finalData = {}) => {
+  const response = await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(finalData),
+  });
+  try {
+    const newData = await response.json();
+    return newData;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// get data from express server and update HTML elements
+const getData = async () => {
+  const response = await fetch("/all");
+  try {
+    const responseData = await response.json();
+    document.getElementById(
+      "date"
+    ).innerHTML = `<span>Date: </span>${responseData.newDate}`;
+    document.getElementById(
+      "city"
+    ).innerHTML = `<span>City: </span>${responseData.name}`;
+    document.getElementById(
+      "temp"
+    ).innerHTML = `<span>Temp: </span>${responseData.temp} &#176C`;
+    document.getElementById(
+      "content"
+    ).innerHTML = `<span>Feelings: </span>${responseData.feelings}`;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// get data from weather app
+const fetchData = async (code) => {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?zip=${code},&appid=${APIKey}&units=metric`
+    );
+    const responseData = await response.json();
+    if (responseData.cod !== 200) {
+      document.getElementById("error").innerHTML = responseData.message;
+    }
+    // clear error
+    setTimeout(() => (document.getElementById("error").innerHTML = ""), 2000);
+
+    return responseData;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// add eventListner to generateBtn
+generateBtn.addEventListener("click", generateData);
